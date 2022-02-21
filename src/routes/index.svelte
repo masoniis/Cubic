ede2a0fd629e460038a9bae50a1e00ae4d3002dc
masoniis <bott.m@comcast.net>
2022-02-20 20:11:04 -0700

    Created the tictactoe board

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,38 @@
 <script>
 	import * as THREE from 'three';
 	import * as SC from 'svelte-cubed';
+    import {onMount} from 'svelte';
+
+    let board;
+
+    onMount(() => {
+        board = true;
+    });
+
 </script>
 
 <div id="info">Test</div>
 
-​<SC.Canvas antialias background={new THREE.Color('papayawhip')}>
-	<SC.Mesh geometry={new THREE.TetrahedronGeometry()} />
-	<SC.PerspectiveCamera position={[1, 1, 3]} />
-+	<SC.OrbitControls enableZoom={true} />
-​</SC.Canvas>
+{#if board}
+<SC.Canvas antialias background={new THREE.Color('papayawhip')}>
+	<SC.Group>
+		<SC.Mesh geometry={new THREE.BoxGeometry()} scale={[0.15, 3, 0.15]} position={[0.5, 0, 0]} />
+		<SC.Mesh geometry={new THREE.BoxGeometry()} scale={[0.15, 3, 0.15]} position={[-0.5, 0, 0]} />
+		<SC.Mesh geometry={new THREE.BoxGeometry()} scale={[3, 0.15, 0.15]} position={[0, 0.5, 0]} />
+		<SC.Mesh geometry={new THREE.BoxGeometry()} scale={[3, 0.15, 0.15]} position={[0, -0.5, 0]} />
+	</SC.Group>
+	<SC.PerspectiveCamera position={[3, 1, -5]} />
+	<SC.OrbitControls enableZoom={true} />
+</SC.Canvas>
+{/if}
 
 <style>
-    #info {
-	position: absolute;
-	top: 10px;
-	width: 100%;
-	text-align: center;
-	z-index: 100;
-	display:block;
-}
+	#info {
+		position: absolute;
+		top: 10px;
+		width: 100%;
+		text-align: center;
+		z-index: 100;
+		display: block;
+	}
 </style>
