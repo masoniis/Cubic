c23952377583c505d3570280550789a8f5bd5891
masoniis <bott.m@comcast.net>
2022-02-20 20:30:45 -0700

    Minor format change

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,56 +2,47 @@
 	import * as THREE from 'three';
 	import * as SC from 'svelte-cubed';
 	import { onMount } from 'svelte';
-	import { fade, slide } from 'svelte/transition';
-    import {tweened} from 'svelte/motion';
-    import {cubicOut} from 'svelte/easing';
+	import { tweened } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
 
-	let board;
 	const scale = tweened(0, {
-        duration: 800,
-        easing: cubicOut
-    });
-
-	onMount(() => {
-		board = true;
-
-        scale.set(3)
+		duration: 1100,
+		easing: cubicOut
 	});
 
+	onMount(() => {
+		scale.set(3);
+	});
 </script>
 
 <div id="info">{$scale}</div>
 
-{#if board}
-	<wrapper in:fade={{ duration: 1 }}>
-		<SC.Canvas antialias background={new THREE.Color('papayawhip')}>
-			<SC.Group>
-				<SC.Mesh
-					geometry={new THREE.BoxGeometry()}
-					scale={[0.15, $scale, 0.15]}
-					position={[0.5, 0, 0]}
-				/>
-				<SC.Mesh
-					geometry={new THREE.BoxGeometry()}
-					scale={[0.15, $scale, 0.15]}
-					position={[-0.5, 0, 0]}
-				/>
-				<SC.Mesh
-					geometry={new THREE.BoxGeometry()}
-					scale={[$scale, 0.15, 0.15]}
-					position={[0, 0.5, 0]}
-				/>
-				<SC.Mesh
-					geometry={new THREE.BoxGeometry()}
-					scale={[$scale, 0.15, 0.15]}
-					position={[0, -0.5, 0]}
-				/>
-			</SC.Group>
-			<SC.PerspectiveCamera position={[3, 1, -5]} />
-			<SC.OrbitControls enableZoom={true} />
-		</SC.Canvas>
-	</wrapper>
-{/if}
+<SC.Canvas antialias background={new THREE.Color('papayawhip')}>
+	<SC.Group>
+		<SC.Mesh
+			geometry={new THREE.BoxGeometry()}
+			scale={[0.15, $scale, 0.15]}
+			position={[0.5, 0, 0]}
+		/>
+		<SC.Mesh
+			geometry={new THREE.BoxGeometry()}
+			scale={[0.15, $scale, 0.15]}
+			position={[-0.5, 0, 0]}
+		/>
+		<SC.Mesh
+			geometry={new THREE.BoxGeometry()}
+			scale={[$scale, 0.15, 0.15]}
+			position={[0, 0.5, 0]}
+		/>
+		<SC.Mesh
+			geometry={new THREE.BoxGeometry()}
+			scale={[$scale, 0.15, 0.15]}
+			position={[0, -0.5, 0]}
+		/>
+	</SC.Group>
+	<SC.PerspectiveCamera position={[3, 1, -5]} />
+	<SC.OrbitControls enableZoom={true} />
+</SC.Canvas>
 
 <style>
 	#info {
