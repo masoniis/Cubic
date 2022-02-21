a828ce895d43a4755b17f48c0dc41198e033a195
masoniis <bott.m@comcast.net>
2022-02-21 10:54:48 -0700

    Clickable grid squares with raycasting, deltetes

--- src/lib/components/GridSlots.svelte
+++ src/lib/components/GridSlots.svelte
@@ -18,88 +18,60 @@
 		scale.set($scale * -1);
 	}
 
-	let show = true;
-	function click() {
-		console.log('BAR CLICKED I REPEAT BAR CLICKED');
-		show = false;
-	}
-
-	const positions = [
-		{ x: -1, y: 1 },
-		{ x: 0, y: 1 },
-		{ x: 1, y: 1 },
-		{ x: -1, y: 0 },
-		{ x: 0, y: 0 },
-		{ x: 1, y: 0 },
-		{ x: -1, y: -1 },
-		{ y: -1, x: 0 },
-		{ x: 1, y: -1 }
+	const squares = [
+		{ position: { x: -1, y: 1 }, visible: true },
+		{ position: { x: 0, y: 1 }, visible: true },
+		{ position: { x: 1, y: 1 }, visible: true },
+		{ position: { x: -1, y: 0 }, visible: true },
+		{ position: { x: 0, y: 0 }, visible: true },
+		{ position: { x: 1, y: 0 }, visible: true },
+		{ position: { x: -1, y: -1 }, visible: true },
+		{ position: { y: -1, x: 0 }, visible: true },
+		{ position: { x: 1, y: -1 }, visible: true }
 	];
 
-	let geometry = BoxBufferGeometry;
-	function test() {
-		console.log('Clicked');
+	function clickHandler(e) {
+		squares[e].visible = false;
+
+		newsquares = squares;
 	}
+
+	$: newsquares = squares;
 </script>
 
 <Group>
-	<Mesh
-		castShadow
-		geometry={new geometry($scale * 0.6, $scale * 0.6, 0.15)}
-		position={positions[0]}
-		material={new MeshStandardMaterial({ color: '#ff3e00' })}
-		interactive
-		on:click={() => test()}
-	/>
+	<!-- {#if box}
+		<Mesh
+			castShadow
+			geometry={new BoxBufferGeometry($scale * 0.6, $scale * 0.6, 0.15)}
+			position={positions[0]}
+			material={new MeshStandardMaterial({ color: '#ff3e00' })}
+			interactive
+			on:click={() => test()}
+		/>
+	{:else}
+		<Mesh
+			castShadow
+			geometry={new CircleBufferGeometry(0.25, 72)}
+			position={squares.positions[0]}
+			material={new MeshStandardMaterial({ color: '#ff3e00' })}
+			interactive
+			on:click={() => test()}
+		/>
+	{/if} -->
 
-	<Mesh
-		castShadow
-		geometry={new BoxBufferGeometry($scale * 0.6, $scale * 0.6, 0.15)}
-		position={positions[1]}
-		material={new MeshStandardMaterial({ color: '#ff3e00' })}
-	/>
-
-	<Mesh
-		castShadow
-		geometry={new BoxBufferGeometry($scale * 0.6, $scale * 0.6, 0.15)}
-		position={positions[2]}
-		material={new MeshStandardMaterial({ color: '#ff3e00' })}
-	/>
-
-	<Mesh
-		castShadow
-		geometry={new BoxBufferGeometry($scale * 0.6, $scale * 0.6, 0.15)}
-		position={positions[3]}
-		material={new MeshStandardMaterial({ color: '#ff3e00' })}
-	/>
-	<Mesh
-		castShadow
-		geometry={new BoxBufferGeometry($scale * 0.6, $scale * 0.6, 0.15)}
-		position={positions[4]}
-		material={new MeshStandardMaterial({ color: '#ff3e00' })}
-	/>
-	<Mesh
-		castShadow
-		geometry={new BoxBufferGeometry($scale * 0.6, $scale * 0.6, 0.15)}
-		position={positions[5]}
-		material={new MeshStandardMaterial({ color: '#ff3e00' })}
-	/>
-	<Mesh
-		castShadow
-		geometry={new BoxBufferGeometry($scale * 0.6, $scale * 0.6, 0.15)}
-		position={positions[6]}
-		material={new MeshStandardMaterial({ color: '#ff3e00' })}
-	/>
-	<Mesh
-		castShadow
-		geometry={new BoxBufferGeometry($scale * 0.6, $scale * 0.6, 0.15)}
-		position={positions[7]}
-		material={new MeshStandardMaterial({ color: '#ff3e00' })}
-	/>
-	<Mesh
-		castShadow
-		geometry={new BoxBufferGeometry($scale * 0.6, $scale * 0.6, 0.15)}
-		position={positions[8]}
-		material={new MeshStandardMaterial({ color: '#ff3e00' })}
-	/>
+	{#each newsquares as { position }, i}
+		{#if squares[i].visible}
+			<Mesh
+				castShadow
+				geometry={new BoxBufferGeometry($scale * 0.6, $scale * 0.6, 0.15)}
+				{position}
+				material={new MeshStandardMaterial({ color: '#ff3e00' })}
+				interactive
+				on:click={() => {
+					clickHandler(i);
+				}}
+			/>
+		{/if}
+	{/each}
 </Group>
