d2ca0d827b84e7c1f03969b3ee5e8f2f57375963
masoniis <bott.m@comcast.net>
2022-02-20 19:06:22 -0700

    Initial testing commit with cube

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script>
+	import * as THREE from 'three';
+	import * as SC from 'svelte-cubed';
+</script>
+
+<h1 class="text-4xl"> HEY </h1>
+
+<SC.Canvas>
+	<SC.Mesh geometry={new THREE.BoxGeometry()} />
+	<SC.PerspectiveCamera position={[1, 1, 3]} />
+</SC.Canvas>
