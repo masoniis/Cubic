49795902adb38ef238ea1a0ee1c158a6a505c3d3
masoniis <bott.m@comcast.net>
2022-02-20 19:54:07 -0700

    Overlay of text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,9 +3,21 @@
 	import * as SC from 'svelte-cubed';
 </script>
 
-<h1 class="text-4xl"> HEY </h1>
+<div id="info">Test</div>
 
-<SC.Canvas>
-	<SC.Mesh geometry={new THREE.BoxGeometry()} />
+​<SC.Canvas antialias background={new THREE.Color('papayawhip')}>
+	<SC.Mesh geometry={new THREE.TetrahedronGeometry()} />
 	<SC.PerspectiveCamera position={[1, 1, 3]} />
-</SC.Canvas>
++	<SC.OrbitControls enableZoom={true} />
+​</SC.Canvas>
+
+<style>
+    #info {
+	position: absolute;
+	top: 10px;
+	width: 100%;
+	text-align: center;
+	z-index: 100;
+	display:block;
+}
+</style>
