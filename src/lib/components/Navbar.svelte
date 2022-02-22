064b069a6a204fa20e188635726fc8143c550064
masoniis <bott.m@comcast.net>
2022-02-22 13:36:15 -0700

    Testing out socket.io

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,6 +1,7 @@
 <main class="bg-slate-400 z-[100] w-full fixed py-6">
 	<flex class="flex twcontainer justify-between mx-auto w-full top-4 z-[100] text-2xlfixed">
 		<a href="/"> Home</a>
+		<a href="/socket">Socket Tests</a>
 		<a href="/mobile"> Mobile friend</a>
 	</flex>
 </main>
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import path from 'path';
+import { Server } from 'socket.io';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -19,7 +20,32 @@ const config = {
 					$components: path.resolve('./src/lib/components'),
 					$stores: path.resolve('./src/stores.ts')
 				}
-			}
+			},
+			plugins: [
+				{
+					name: 'sveltekit-socket-io',
+					configureServer(server) {
+						const io = new Server(server.httpServer);
+
+						io.on('connection', (socket) => {
+							// Generate a random username and send it to the client to display it
+							let username = `User ${Math.round(Math.random() * 999999)}`;
+							socket.emit('name', username);
+
+							// Receive incoming messages and broadcast them
+							socket.on('message', (message) => {
+								io.emit('message', {
+									from: username,
+									message: message,
+									time: new Date().toLocaleString()
+								});
+							});
+						});
+
+						console.log('SocketIO injected');
+					}
+				}
+			]
 		}
 	}
 };
